23d72faebfdd72217c170f858c4e85012137069c
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2022-09-29 20:25:19 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -11,13 +11,13 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.print("  a b c d e f g h");
+		System.out.print("  abcdefgh");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
 		
 		if(piece == null) {
-			System.out.print("- ");
+			System.out.print("-");
 		}else {
 			System.out.print(piece);
 		}

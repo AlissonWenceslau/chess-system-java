54c32f3488f83798f486212b60d09d3ee7a02bd4
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2022-10-27 20:31:20 -0300

    Fixed black pawn first move

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -57,7 +57,7 @@ public class Pawn extends ChessPiece {
 			
 			//Testing two line above
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPeace(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPeace(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
